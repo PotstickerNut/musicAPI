e587f45c2124a1e9f19369d197f396e0e533bfdd
Janette Campbell <campbjp@hotmail.com>
2022-06-17 11:49:47 -0400

    added more dependencies and middleware

--- routes/musicRouter.js
+++ routes/musicRouter.js
@@ -1,2 +1,14 @@
 const express = require("express");
 const musicModel = require("../models/musicSchema");
+
+const router = express.Router();
+
+// ======= Get songs =======
+router.get("/", async (req, res) => {
+  try {
+    const song = musicModel.find();
+    res.status(200).json(song);
+  } catch (error) {
+    console.error(error);
+  }
+});
